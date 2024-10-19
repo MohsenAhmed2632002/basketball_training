33054c1f896d1e58a0f54219fd2e69bbba9c8a2b
MohsenAhmed2632002 <sphinx2632002@gmail.com>
2024-10-19 03:57:41 +0300

    ModelAI

--- lib/features/Home/presentation/pages/Homepage.dart
+++ lib/features/Home/presentation/pages/Homepage.dart
@@ -1,16 +1,15 @@
-import 'package:awesome_snackbar_content/awesome_snackbar_content.dart';
-import 'package:basketball_training/core/Theming/Font.dart';
-import 'package:basketball_training/core/Widgets/Shared_Widgets.dart';
-import 'package:basketball_training/features/Home/data/models/HomeModel.dart';
-import 'package:basketball_training/features/Home/data/models/MyLocationModel.dart';
 import 'package:basketball_training/features/Home/presentation/cubit/home_cubit.dart';
 import 'package:basketball_training/features/Home/presentation/cubit/home_state.dart';
 import 'package:basketball_training/features/Home/presentation/widgets/HomePageWidgets.dart';
-import 'package:fl_chart/fl_chart.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:geolocator/geolocator.dart';
+import 'package:flutter/foundation.dart';
+import 'package:http/http.dart' as http;
+import 'dart:convert';
+
+import '../../../../core/Theming/Font.dart';
 
 class HomeScreen extends StatefulWidget {
   const HomeScreen({super.key});
@@ -49,6 +48,32 @@ class _HomeScreenState extends State<HomeScreen> {
     }
   }
 
+  Future<void> getPrediction(List<int> features) async {
+    final url = Uri.parse('http://192.168.1.9:5001/predict');
+
+    // Create the POST request body
+    Map<String, dynamic> body = {'features': features};
+
+    // Send the POST request
+    final response = await http.post(
+      url,
+      headers: {'Content-Type': 'application/json'},
+      body: json.encode(body),
+    );
+
+    // Handle the response
+    if (response.statusCode == 200) {
+      final prediction = json.decode(response.body)['prediction'];
+      if (kDebugMode) {
+        print('Prediction: $prediction');
+      }
+    } else {
+      if (kDebugMode) {
+        print('Failed to get prediction');
+      }
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -56,7 +81,7 @@ class _HomeScreenState extends State<HomeScreen> {
         child: SingleChildScrollView(
           child: Container(
             width: MediaQuery.of(context).size.width,
-            height: MediaQuery.of(context).size.height - 180.h,
+            // height: MediaQuery.of(context).size.height - 180.h,
             child: Column(
               // mainAxisAlignment: MainAxisAlignment.spaceBetween,
               // crossAxisAlignment: CrossAxisAlignment.center,
@@ -92,6 +117,35 @@ class _HomeScreenState extends State<HomeScreen> {
                             ContainerForecast(
                               myWeatherResponseModel:
                                   MyWeatherResponseModel.forecast,
+                            ),
+                            IconButton(
+                              onPressed: () {
+                                //- outlook is rainy (the value is 0 for no and 1 for yes)
+                                // - outlook is sunny (the value is 0 for no and 1 for yes)
+                                // - temperature is hot (the value is 0 for no and 1 for yes)
+                                // - temperature is mild (the value is 0 for no and 1 for yes)
+                                // - humidity is normal (the value is 0 for no and 1 for yes)
+                                List<int> features_0 = [
+                                  0,
+                                  1,
+                                  1,
+                                  0,
+                                  0
+                                ]; // Prediction: [0]
+                                List<int> features_1 = [
+                                  0,
+                                  1,
+                                  0,
+                                  1,
+                                  1
+                                ]; // Prediction: [1]
+                                getPrediction(
+                                    features_1); // Call getPrediction with the feature list
+                              },
+                              icon: const Icon(
+                                Icons.get_app_sharp,
+                                size: 50,
+                              ),
                             ),
                           ],
                         );
